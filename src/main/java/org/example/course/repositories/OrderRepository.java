715375c3128291774eedb56dc19b14331af2e61a
Simão Baltazar <simaoraposobaltazar@gmail.com>
2026-09-11 14:34:46 +0100

    Order, Instant, ISO 8601

---
+++ src/main/java/org/example/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package org.example.course.repositories;
+
+
+import org.example.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
